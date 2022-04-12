19307594c90346c0a4cb10db3675917af9c0860b
Mufaroe04 <83914739+Mufaroe04@users.noreply.github.com>
2022-04-12 14:58:57 +0200

    Update upload_app.py

--- upload_app.py
+++ upload_app.py
@@ -56,7 +56,7 @@ def video_to_frames(input_loc, output_loc):
             print ("Done extracting frames.\n%d frames extracted" % count)
             print ("It took %d seconds forconversion." % (time_end-time_start))
             break
-    def obj_det(search, frame, model):
+def obj_det(search, frame, model):
     label = secpred(frame, model)
     label = label.lower()
     if label.find(search) > -1:
